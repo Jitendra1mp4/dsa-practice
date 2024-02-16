c6a89510767638dc2a25f8c3a770bf7e0253d8d7
Jitendra Kumar <jitendrasahu6761@gmail.com>
2024-02-16 23:32:17 +0530

    doc : formate code

--- creatingANode.c
+++ creatingANode.c
@@ -6,28 +6,29 @@ struct node {
   Node *link;
 };
 
+
 int main(int argc, char const *argv[]) {
   Node *head = NULL;
   Node *current = (Node *)malloc(sizeof(Node));
-  current->data = 5 ;
-  current->link = NULL ;
-  head = current ;
+  current->data = 5;
+  current->link = NULL;
+  head = current;
 
-  printf("data of node1 : %d\n",head->data) ;
+  printf("data of node1 : %d\n", head->data);
 
-  current = (Node *)malloc(sizeof(Node)) ;
-  current->data = 6 ;
-  current->link = NULL ;
+  current = (Node *)malloc(sizeof(Node));
+  current->data = 6;
+  current->link = NULL;
 
-  head->link = current ;
-  printf("data of node2 : %d\n",head->link->data);
+  head->link = current;
+  printf("data of node2 : %d\n", head->link->data);
 
+  current = (Node *)malloc(sizeof(Node));
+  current->data = 12;
+  current->link = NULL;
+  head->link->link = current;
+  printf("data of node3 : %d\n", head->link->link->data);
 
-  current = (Node *) malloc(sizeof(Node)) ;
-  current->data = 12 ;
-  current->link = NULL ;
-  head->link->link = current ;
-  printf("data of node3 : %d\n",head->link->link->data);
 
   return 0;
 }
